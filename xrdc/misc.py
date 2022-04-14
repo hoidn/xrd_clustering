4fe0630108a16e56847102154f57bc88982bf88e
ollie <ollie@localhost.localdomain>
2022-04-14 01:17:30 +0000

    updated example for casette data peak-fitting

--- xrdc/misc.py
+++ xrdc/misc.py
@@ -2,8 +2,15 @@
 Miscelaneous utilities
 """
 
+import numpy as np
+
 def deepmap(lst, func, level):
     if level <= 0:
         return func(lst)
     else:
         return [deepmap(sub, func, level - 1) for sub in lst]
+
+def transpose_x_3d(arr):
+    return np.swapaxes(
+            np.swapaxes(arr, 0, 2), 
+                0, 1)
