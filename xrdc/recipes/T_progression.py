9641e3bc69ddc7642af07edcb64b788f4f4b5cb9
ollie <ollie@localhost.localdomain>
2022-05-02 23:03:33 +0000

    Refactor prior sampling function in phase_mixture_toy_model_simplified.ipynb

---
+++ xrdc/recipes/T_progression.py
@@ -0,0 +1,75 @@
+import numpy as np
+from xrdc import xrdutils as xdu
+from xrdc import simulation as sim
+
+from xrdc import simulation
+from xrdc import datasets as xd
+import matplotlib.pyplot as plt
+
+from .. import featurization as feat
+from .. import source_separation as sep
+from .. import peak_fitting as pf
+from ..datasets import d2d
+
+def sample_peak_intensity():
+    return np.random.uniform()
+
+# https://stackoverflow.com/questions/35282456/find-local-maximums-in-numpy-array
+import pandas as pd
+from scipy.signal import argrelextrema
+
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+
+
+def get_ridges(orig, axis = 1):
+    # determine the indices of the local maxima
+    max_ind = argrelextrema(orig, np.greater, axis = axis)
+#     max_ind_2 = argrelextrema(orig, np.greater, axis = 0)
+#     max_ind = np.hstack((max_ind[0], max_ind_2[0])), np.hstack((max_ind[1], max_ind_2[1]))
+    
+    edges = np.zeros_like(orig)
+    edges[max_ind] = 1
+    return edges
+
+def shuffle(bin_img, size = 1):
+    ret = np.zeros_like(bin_img)
+    for s in range(-size, size + 1):
+        ret += np.roll(bin_img, s, axis = 0)
+        ret += np.roll(bin_img, s, axis = 1)
+    return np.sign(ret)
+
+patterns = d2d.patterns
+slow_q, fast_q, slow_T, fast_T = sep.separate_signal(patterns, cutoff = .25,  threshold = 50, smooth_q = 1.7)
+background = slow_q
+
+def default_featurize(patterns_pp):
+    #patterns_pp = gf(patterns, (1, 1.7))
+    labeled, feature_masks, activations, norm_, activations_n1 = feat.get_ridge_features(
+        patterns_pp,
+       smooth_ax1 = 'FWHM', smooth_ax0 = 1, threshold_percentile = 75, thicken = True, size_thresh = 5, bgsub=False,
+        log_scale_features = False, fwhm_finder=feat.fwhm_finder, do_flood_thicken = False, max_size_flood = 20,
+        thicken_ax0 = 0, thicken_ax1 = 1, flood_threshold=.95, smooth_factor_ax1 = .125)
+    return labeled, feature_masks, activations, norm_, activations_n1
+
+patterns_pp_simple = fast_q * (fast_q > 0)
+labeled_simple, feature_masks_simple, activations_simple, _norm, activations_n1_simple =\
+    default_featurize(patterns_pp_simple);
+# straightened_heatmap_simple = feat.get_peakshift_corrected_heatmap(patterns_pp_simple, activations_simple, labeled_simple)
+
+def get_activations(patterns_pp_simple):
+    activations_n1_simple = feat.norm((feature_masks_simple * patterns_pp_simple).sum(axis = 2), axis = 1)
+    return activations_n1_simple
+
+act = get_activations(fast_q * (fast_q > 0))
+
+simtype = 'Cosine'
+scaling = 'log'
+linkage = 'ward'
+ctype = 'agglom'
+
+feature_csims1, o_cuts = feat.sims_with_boundaries(patterns, act, act, n = 7, simtype = simtype, extra_label='',
+                    ctype = ctype, linkage = linkage, affinity = 'euclidean')
+
+#fsub_stop_2d = pf.curvefit_2d((patterns - 0), stdratio_threshold = 2, noise_estimate = fast_T,
+#                   background = background, bg_shift_pos = False)
