3b300ea1c7aea0e29dccce5032478a5a771e2672
O H <ohoidn@slac.stanford.edu>
2023-01-24 17:45:31 -0500

    Link pairwise mean differences to p values using bootstrapping and empirical CDFs
    This requires converting every bootstrap sample ensemble into a CDF,
    instead of just reducing it into its mean and standard deviation.

--- xrdc/noise_analysis.py
+++ xrdc/noise_analysis.py
@@ -24,8 +24,33 @@ def resample_2d(arr):
 def boot_stderr(arr2d):
     return np.vstack([resample_2d(arr2d).mean(axis = 0) for _ in range(100)]).std(axis = 0)
 
-def boot_mean(arr2d):
-    return np.vstack([resample_2d(arr2d).mean(axis = 0) for _ in range(100)]).mean(axis = 0)
+from statsmodels.distributions.empirical_distribution import ECDF
+
+#def boot_cdfs(arr2d):
+#    """
+#    return mean-subtracted CDFs
+#    """
+#    samples = np.vstack([resample_2d(arr2d).mean(axis = 0) for _ in range(100)])
+#    means = samples.mean(axis = 0)
+#    centered = np.abs(samples - means)
+#    cdfs = np.array([ECDF(centered[:, i]) for i in range(samples.shape[1])])
+#    return cdfs
+#def boot_pvalue(arr2d, signed_zscore, cdfs):
+#    stderr = reduce_stderr(pairwise_stderr)
+#    #diffs = pairwise_diffs.mean(axis = 0)
+#    print('pval', arr2d.shape, signed_zscore.shape)
+#    #deviations = signed_zscore * stderr
+#    cdfs = boot_cdfs(np.abs(pairwise_diffs))
+#    tails_values = np.array([cdf(dev) for cdf, dev in zip(cdfs, deviations)])
+#    return 1 - tails_values
+#    #return 2 * np.min([tails_values, 1 - tails_values], axis = 0)
+
+def boot_mean(arr2d, condense = True):
+    samples = np.vstack([resample_2d(arr2d).mean(axis = 0) for _ in range(100)])
+    if condense:
+        return samples.mean(axis = 0)
+    else:
+        return samples
 
 def pairdiffs(arr2d):
     return arr2d[:, None, :] - arr2d[..., None]
@@ -42,10 +67,25 @@ def _boot_diffs_pairwise(arr2d, N = 60):
     boot_mean = boot_sample.mean(axis = 0)
     return boot_mean
 
-def boot_diffs_pairwise(arr2d):
+# TODO maintain bootstrap distribution, mask without aggregating
+# or, easier, make the independence assumption
+def boot_diffs_pairwise(arr2d, agg_dim = 0, mask = None):
     print('pairwise', arr2d.shape)
     samples = np.array([_boot_diffs_pairwise(arr2d) for _ in range(100)])
-    return samples.mean(axis = 0), samples.std(axis = 0)
+    if agg_dim == 0: # condense into bootstrap means and standard deviations
+        return samples.mean(axis = 0), samples.std(axis = 0)
+    elif agg_dim in (1, 2): # retain all the samples, but sum in one of the block index dimensions
+        return (samples * mask).sum(axis = agg_dim)
+#        cdfs = np.array(
+#                [[ECDF(samples[:, i, j])
+#                    for i in range(samples.shape[0])]
+#                for j in range(samples.shape[1])]
+#            )
+    else:
+        raise ValueError
+
+#def agg_cdf(samples_b_j, mask):
+
 
 
 def c2d(arr, kernel):
@@ -62,7 +102,6 @@ def get_aggregate_std(arr, kernel, blocksize):
     idxs = np.arange(len(arr))
     return idxs[::blocksize], np.std(arr_conv[:, ::blocksize], axis = 0)
 
-
 def get_aggregate_stderr(arr, kernel, blocksize):
     idxs, arr_conv = c2d(arr, kernel)
     return idxs[::blocksize], arr_conv[:, ::blocksize],\
@@ -76,7 +115,17 @@ def cartesian_diff(a, b):
     b = b.reshape((b.shape[0], 1))
     return b - a
 
-def get_agged_zscores(fast_q, peak, blocksize, mask, mode = 'agg_first'):
+def reduce_stderr(arr2d):
+    return np.sqrt((arr2d**2 + 1e-9).sum(axis = 0))
+
+def cdfs_from_boot_samples(arr2d, sample_axis = 0):
+    assert sample_axis == 0
+    return np.array([ECDF(arr2d[:, i] * np.sign(arr2d[:, i].mean()))
+        for i in range(arr2d.shape[1])])
+
+def get_agged_zscores(fast_q, peak, blocksize, mask, mode = 'agg_first',
+        signed = False):
+    print('signed', signed)
     if mode == 'agg_first':
         _, agged, mean, sterr = get_aggregate_stderr(fast_q, peak, blocksize)
         pairwise_diffs = cartesian_diff(mean, mean) * mask
@@ -86,11 +135,27 @@ def get_agged_zscores(fast_q, peak, blocksize, mask, mode = 'agg_first'):
         arr_conv = arr_conv[:, ::blocksize]
         pairwise_diffs, pairwise_stderr = boot_diffs_pairwise(arr_conv)
         pairwise_diffs, pairwise_stderr = pairwise_diffs * mask, pairwise_stderr * mask
+    elif mode == 'cdf':
+        _, arr_conv = c2d(fast_q, peak)
+        arr_conv = arr_conv[:, ::blocksize]
+        pairwise_diffs, pairwise_stderr = boot_diffs_pairwise(arr_conv)
+        pairwise_diffs, pairwise_stderr = pairwise_diffs * mask, pairwise_stderr * mask
+        samples_j = boot_diffs_pairwise(arr_conv, agg_dim = 1, mask = mask)
+        cdfs = cdfs_from_boot_samples(samples_j)
+        if not signed:
+            # TODO standard error aggregation wrong? TODO
+            zscores = (np.sum(np.abs(pairwise_diffs), axis = 0) / (pairwise_stderr + 1e-9).sum(axis = 0))
+        else:
+            zscores = (np.sum((pairwise_diffs), axis = 0) / (pairwise_stderr + 1e-9).sum(axis = 0))
+        return zscores, cdfs
     else:
         raise ValueError
-    zscores = (np.sum(np.abs(pairwise_diffs), axis = 0) / (pairwise_stderr + 1e-9).sum(axis = 0))
-    return zscores
-
+    if not signed:
+        # TODO standard error aggregation wrong? TODO
+        zscores = (np.sum(np.abs(pairwise_diffs), axis = 0) / (pairwise_stderr + 1e-9).sum(axis = 0))
+    else:
+        zscores = (np.sum((pairwise_diffs), axis = 0) / (pairwise_stderr + 1e-9).sum(axis = 0))
+    return zscores, (pairwise_diffs, pairwise_stderr)
 
 def cmp_zscores(fast_q, scores, blocksize):
     a, b = 2, 1
@@ -128,13 +193,13 @@ def plot_zscores_subsample_rows(fast_q, row_blocksize = 1, blocksize = 10, peak 
 
     if not transpose:
         cmp_zscores(fast_q[::row_blocksize], get_agged_zscores(
-            fast_q[::row_blocksize], peak, blocksize, off_diag_mask, **kwargs),
+            fast_q[::row_blocksize], peak, blocksize, off_diag_mask, **kwargs)[0],
             blocksize)
     else:
         off_diag_mask = (np.roll(np.eye(len(fast_q[0][::blocksize].T)), -1, axis = 0) +\
             np.roll(np.eye(len(fast_q[0][::blocksize].T)), 1, axis = 0)).astype(bool)
         cmp_zscores(fast_q[::row_blocksize].T, get_agged_zscores(
-            fast_q[::row_blocksize].T, peak.T, 1, off_diag_mask, **kwargs),
+            fast_q[::row_blocksize].T, peak.T, 1, off_diag_mask, **kwargs)[0],
             blocksize)
 
 
@@ -159,7 +224,7 @@ def zipmerge(arrs, shape):
 
 def padded_zscores_shifted(fast_q, bs, peak, shift = 0, **kwargs):
     fast_q = np.roll(fast_q, shift, axis = 1)
-    arr = plot_z_blocksize(fast_q, bs, peak, plot = False, **kwargs)
+    arr, pairwise = plot_z_blocksize(fast_q, bs, peak, plot = False, **kwargs)
     return arr
 
 def padded_zscores(fast_q, stride, peak, shifts = False, **kwargs):
@@ -167,18 +232,16 @@ def padded_zscores(fast_q, stride, peak, shifts = False, **kwargs):
     shape = fast_q.shape[1]
     if not shifts:
         # TODO fix this case. Need to call get_agged_zscores
-        arr = plot_z_blocksize(fast_q, stride, peak, plot = False, **kwargs)
+        arr, pairwise_stats = plot_z_blocksize(fast_q, stride, peak, plot = False, **kwargs)
         #pad = stride - (len(arr) % stride)# - (shape % stride)#shape - len(arr)
         qpoints = np.arange(len(arr) * stride).reshape((-1, stride)).T.mean(axis = 0)
         fn = interp1d(qpoints, arr, fill_value = 'extrapolate')
-        return fn(np.arange(shape))
-
+        return fn(np.arange(shape)), pairwise_stats
     else:
         res = []
         for shift in range(stride):
             res.append(padded_zscores_shifted(fast_q, stride, peak, shift = shift, **kwargs))
-        return zipmerge(res, shape)
-
+        return zipmerge(res, shape), pairwise_stats
 
 def plot_z_blocksize(fast_q, blocksize, peak, plot = True, **kwargs):
     padded = np.pad(fast_q[0], (0, blocksize - (len(fast_q[0]) % blocksize)), mode = 'edge')
@@ -186,11 +249,11 @@ def plot_z_blocksize(fast_q, blocksize, peak, plot = True, **kwargs):
     off_diag_mask = (np.roll(np.eye(len(fast_q[0][::blocksize])), -1, axis = 0) +\
                 np.roll(np.eye(len(fast_q[0][::blocksize])), 1, axis = 0)).astype(bool)
 
-    zloc = get_agged_zscores(fast_q, peak, blocksize, off_diag_mask, **kwargs)
+    zloc, pairwise_stats = get_agged_zscores(fast_q, peak, blocksize, off_diag_mask, **kwargs)
     #plt.plot(qpoints[:len(zloc)], zloc, label = blocksize)
     if plot:
         plt.plot(qpoints[:len(zloc)], np.log(1 + zloc), label = blocksize)
-    return zloc
+    return zloc, pairwise_stats
 
 def mk_peak_blocksize(arr2d, bs, ptype = 'gaussian'):
     """
@@ -385,6 +448,76 @@ def plot_emv_zscores(arr2d, z_vs_blocksize, q, aspect = 18.3,
         idxs, arr_conv = c2d(arr2d, kernel)
         stderr = boot_stderr(arr_conv[:, ::bs])
         plt.plot(q[::bs], stderr + i * scale1, label = 'block size {}'.format(bs))
+    plt.grid()
+    plt.ylim(0, ymax)
+    #plt.xlim(0, shape - 1)
+    plt.legend()
+    plt.autoscale(enable=True, axis='x', tight=True)
+    plt.title('(b)')
+    xticks = ax1.xaxis.get_major_ticks()
+    xticks[0].label1.set_visible(False)
+
+    ax2 = plt.subplot(a, b, 2)
+    #plt_lineout(np.abs(arr2d).mean(axis = 0), 3 * offset_scale, label = 'raw (averaged)')
+    for i, (idx, label) in enumerate(zip(lineout_indices, lineout_labels)):
+        plt_lineout(z_vs_blocksize[idx], i * offset_scale, label = 'log (1 + Z score), block size {}'.format(label))
+    plt.grid()
+    plt.ylim(0, ymax)
+    plt.xlim(0, shape - 1)
+    plt.legend(loc = 'upper right')
+    plt.title('(a)')
+    xticks = ax2.xaxis.get_major_ticks()
+    xticks[0].label1.set_visible(False)
+
+    plt.savefig('zscores_60.pdf')
+
+def twopanel_generic(sterr_vs_blocksize, z_vs_blocksize, q, aspect = 18.3,
+        block_sizes = [2, 4, 8, 16], lineout_labels = [2, 4, 8, 16], offset_scale = 2.,
+        ymax = 19, do_top = True, do_bottom = True, lineout_indices = [1, 3, 7]):
+
+    shape = sterr_vs_blocksize.shape[1]
+    # matplotlib.rcParams['figure.figsize'] =(18, 9)
+
+    best = z_vs_blocksize[9]
+
+    size2 = 18
+    size1 = 15
+    plt.rcParams.update({'font.size': size1})
+    plt.rcParams.update({'axes.labelsize': size2})
+    plt.rcParams.update({'axes.titlesize': size2})
+    plt.rcParams.update({'xtick.labelsize': size2})
+    plt.rcParams.update({'ytick.labelsize': size2})
+
+
+    plt.savefig('zscores_60.pdf')
+
+    def plt_lineout(lineout, offset = 0, **kwargs):
+        #- lineout.min()
+        plt.plot((np.log(1 + lineout - lineout.min()) + offset), **kwargs)
+
+    a, b = 2, 1
+#    ax1 = plt.subplot(a, b, 1)
+#    scale1 = 1
+#    for i, bs in enumerate(block_sizes):
+#        print(i)
+#        kernel = mk_square(arr2d, bs)
+#        idxs, arr_conv = c2d(arr2d, kernel)
+#        stderr = boot_stderr(arr_conv[:, ::bs])
+#        plt.plot(q[::bs], stderr + i * scale1, label = 'block size {}'.format(bs))
+#    plt.grid()
+#    plt.ylim(0, ymax)
+#    #plt.xlim(0, shape - 1)
+#    plt.legend()
+#    plt.autoscale(enable=True, axis='x', tight=True)
+#    plt.title('(a)')
+#    xticks = ax1.xaxis.get_major_ticks()
+#    xticks[0].label1.set_visible(False)
+
+    a, b = 2, 1
+    ax1 = plt.subplot(a, b, 1)
+    scale1 = 1
+    for i, bs in enumerate(block_sizes):
+        plt.plot(sterr_vs_blocksize + i * scale1, label = 'block size {}'.format(bs))
     plt.grid()
     plt.ylim(0, ymax)
     #plt.xlim(0, shape - 1)
